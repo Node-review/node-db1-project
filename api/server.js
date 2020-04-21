8b6e14822fe4b4f2cb806a6487528214e1f2588e
Nhan <natenguyen51294@gmail.com>
2020-04-21 10:51:26 -0700

    project set up

--- api/server.js
+++ api/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
-
-const db = require("../data/dbConfig.js");
+const accountRouters = require('../data/accountsRouter.js');
+const helmet = require('helmet');
+const logger = require('morgan');
 
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
+server.use(logger('dev'));
+
+server.use('/api/accounts', accountRouters);
+
+server.get('/', (req, res) => {
+  res.send({ API: `It's working` })
+});
 
 module.exports = server;
---
+++ data/accountsRouter.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const db = require('./dbConfig.js');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+
+});
+
+router.get('/:id', async (req, res) => {
+
+});
+
+router.post('/', async (req, res) => {
+
+});
+
+router.put('/:id', async (req, res) => {
+
+});
+
+router.delete('/:id', async (req, res) => {
+
+});
+
+module.exports = router;
